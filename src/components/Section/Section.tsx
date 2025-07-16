faa0736c6956bead5335241a47eafe2bb437d664
Hanna Assaf <145717934+HannaAssaf@users.noreply.github.com>
2025-07-16 20:39:43 +0100

    children

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -1,5 +1,8 @@
 import style from "./Section.module.css";
+interface SectionProps {
+  children: React.ReactNode;
+}
 
-export default function Section({ children }) {
+export default function Section({ children }: SectionProps) {
   return <section className={style.section}>{children}</section>;
 }
